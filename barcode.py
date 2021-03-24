98820ffe6ea95bad803e36c853519aac969e0b0e
Saaransh Sharma <43897976+strato67@users.noreply.github.com>
2021-03-23 21:09:25 -0400

    ham
    we added ham

--- barcode.py
+++ barcode.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import os
 
+
 class barcodeGenerator:
     def __init__(self, imgarray):
         self.imgarray = imgarray
@@ -69,10 +70,11 @@ class barcodeGenerator:
 
 class searchAlgorithm:
 
-    def __init__(self,userInput,fileandBarcode):
-        self.userInput=userInput
+    def __init__(self, userInput, fileandBarcode):
+        self.sharyarWithHam = []  # Sharyar eating black forest ham
+        self.userInput = userInput
         self.fileandBarcode = fileandBarcode
-        
+
     def ham(self, x, y):
 
         hd = 0
@@ -81,54 +83,54 @@ class searchAlgorithm:
             if digit1 != digit2:
                 hd += 1
         return hd
-   
+
     def search(self):
         filelocation = ""
-                
         for i in range(len(fileandBarcode)):
-            if str(fileandBarcode[i][1]).replace(",", "").replace(" ", "").replace("[", "").replace("]", "") == userInput:
-                filelocation = fileandBarcode[i][0]
-                print(self.ham(userInput, str(fileandBarcode[i][1]).replace(",", "").replace(" ", "").replace("[", "").replace("]", ""))
-                
-        return filelocation
+            # if str(fileandBarcode[i][1]).replace(",", "").replace(" ", "").replace("[", "").replace("]", "") == userInput:
+            filelocation = fileandBarcode[i][0]
+            hd = self.ham(userInput, str(fileandBarcode[i][1]).replace(
+                ",", "").replace(" ", "").replace("[", "").replace("]", ""))
+            self.sharyarWithHam.append((hd, filelocation))
+            # print(type(fileandBarcode[i][1]))
+
+        sortedSharyarWithHam = sorted(self.sharyarWithHam)
+        for i in range(10):
+            print(sortedSharyarWithHam[i])
+        # print(sortedSharyarWithHam)
+        # print(sortedSharyarWithHam[1][1])
+        # return filelocation
 
 
-    
 if __name__ == "__main__":
 
     filepathList = []
     barcodeList = []
-    
+
     for sub, dirs, files in os.walk(r"..\barcodeScan\MNIST_DS"):
         for filename in files:
             filepath = sub+os.sep+filename
 
             if filepath.endswith(".jpg"):
-                filepathList.append(filepath)       
-
+                filepathList.append(filepath)
 
     for i in range(len(filepathList)):
-            
+
         image = Image.open(filepathList[i])
         imgarray = np.asarray(image)
         barcode = barcodeGenerator(image)
         barcodeList.append(barcode.concatenate())
-    
-    fileandBarcode = list(zip(filepathList,barcodeList))
-    
+
+    fileandBarcode = list(zip(filepathList, barcodeList))
+
     print(fileandBarcode)
-    
+
     userInput = input("Enter a barcode: ")
-    userInput = userInput.replace(" ","").replace(",","")
+    userInput = userInput.replace(" ", "").replace(",", "")
     print(userInput)
-    
-    searching = searchAlgorithm(userInput,fileandBarcode)
-    print(searching.search())
-    #print(searching.ham())
-    #print(fileandBarcode[0][1])
-    #print(imgarray)
+
+    searching = searchAlgorithm(userInput, fileandBarcode)
+    # print(searching.ham("00100", "11011"))
+    searching.search()
 
 
-
-
-    
