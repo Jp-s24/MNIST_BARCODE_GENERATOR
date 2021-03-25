552d66ffde9bee29293ad938c9971c7797acee14
Saaransh Sharma <43897976+strato67@users.noreply.github.com>
2021-03-25 01:06:21 -0400

    added barcode csv

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This project assigns barcodes to a dataset of 100 images from the MNIST database. 
 Using threshold projections and the Hamming distance algorithims, it compares barcodes to find visually similar images within the dataset. 
+
+Instructions: Look for desired barcode in 'filesandbarcode.csv', and input into 'barcode.py'
--- barcode.py
+++ barcode.py
@@ -1,11 +1,14 @@
 from PIL import Image
 import numpy as np
 import os
+import csv
 
+#Generating barcodes for each image
 class barcodeGenerator:
     def __init__(self, imgarray):
         self.imgarray = imgarray
 
+    #Projection 1 of image (0 deg)
     def th1(self):
         rowsums = []
         thresh1 = []
@@ -21,6 +24,7 @@ class barcodeGenerator:
 
         return thresh1
 
+    #Projection 2 of image (45 deg)
     def th2(self):
         thresh2 = []
         right = [np.sum(np.diag(imgarray, d))
@@ -34,6 +38,7 @@ class barcodeGenerator:
                 thresh2.append(1)
         return thresh2
 
+    #Projection 3 of image (90 deg)
     def th3(self):
         rowsums = []
         thresh3 = []
@@ -49,6 +54,7 @@ class barcodeGenerator:
 
         return thresh3
 
+    #Projection 4 of image (135 deg)
     def th4(self):
         thresh4 = []
         left = [np.sum(np.diag(np.fliplr(imgarray), d))
@@ -62,18 +68,20 @@ class barcodeGenerator:
                 thresh4.append(1)
         return thresh4
 
+    #Combine all projections to one barcode
     def concatenate(self):
         newBarcode = self.th1()+self.th2()+self.th3()+self.th4()
         return newBarcode
 
-
+#Searching for similar images
 class searchAlgorithm:
 
     def __init__(self, userInput, fileandBarcode):
-        self.sharyarWithHam = []  # Sharyar eating black forest ham
+        self.fileLocationHam = []  
         self.userInput = userInput
         self.fileandBarcode = fileandBarcode
 
+    #Calculates Hamming distance of each image
     def ham(self, x, y):
 
         hd = 0
@@ -83,6 +91,7 @@ class searchAlgorithm:
                 hd += 1
         return hd
 
+    #Calculates accuracy of top nine closest images from Hamming distance
     def accuracy(self,sortArray):
         filename = str(sortArray[0][1])
         numComp = int(filename[24])
@@ -95,7 +104,7 @@ class searchAlgorithm:
 
         return (counter-1)/9
 
-
+    #Comparing searched image with all other images in database
     def search(self):
         filelocation = ""
         for i in range(len(fileandBarcode)):
@@ -103,42 +112,55 @@ class searchAlgorithm:
             filelocation = fileandBarcode[i][0]
             hd = self.ham(userInput, str(fileandBarcode[i][1]).replace(
                 ",", "").replace(" ", "").replace("[", "").replace("]", ""))
-            self.sharyarWithHam.append((hd, filelocation))
+            self.fileLocationHam.append((hd, filelocation))
             
 
-        sortedSharyarWithHam = sorted(self.sharyarWithHam)
+        sortedfileLocationHam = sorted(self.fileLocationHam)
+        print("\nHamming distance and file location")
         for i in range(10):
-            print(sortedSharyarWithHam[i])
-        print(self.accuracy(sortedSharyarWithHam))
+            print(sortedfileLocationHam[i])
+        return self.accuracy(sortedfileLocationHam)
 
 if __name__ == "__main__":
 
     filepathList = []
     barcodeList = []
 
+    #Loading each image from database
     for sub, dirs, files in os.walk(r"..\barcodeScan\MNIST_DS"):
         for filename in files:
             filepath = sub+os.sep+filename
 
             if filepath.endswith(".jpg"):
                 filepathList.append(filepath)
-
+    
+    #Assigning a barcode to each image in database
     for i in range(len(filepathList)):
 
         image = Image.open(filepathList[i])
         imgarray = np.asarray(image)
         barcode = barcodeGenerator(image)
         barcodeList.append(barcode.concatenate())
-
+        
+    #List stores image file path and its barcode
     fileandBarcode = list(zip(filepathList, barcodeList))
 
-    print(fileandBarcode)
+    #Writing fileandBarcode to csv
+    with open('filesandbarcode.csv','w') as result:
+        w = csv.writer(result,dialect='excel')
+        w.writerows(fileandBarcode)  
 
+    #Getting user's barcode
     userInput = input("Enter a barcode: ")
-    userInput = userInput.replace(" ", "").replace(",", "")
+    userInput = userInput.replace(" ", "").replace(
+        ",", "").replace("[", "").replace("]", "").replace('"','')
 
+    #Performing comparison
     searching = searchAlgorithm(userInput, fileandBarcode)
     
-    searching.search()
+    print("The hit accuracy for the closest 9 results was: ",
+        "{:.0%}".format(searching.search()))
+    
+    
 
 
